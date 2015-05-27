0672bd7c88d2bb4987d0c8c5b48bb104b2bf659e
flowerwrong <sysuyangkang@gmail.com>
2015-05-27 16:35:26 +0800

    Add update banner of index page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -11,8 +11,23 @@ class Admin::DashboardController < Admin::ApplicationController
 
   def update_banners
     @data = YAML.load_file(Rails.root.join('config', 'site.yml'))
-    @data['banner_one']['image_url'] = 'http://static.segmentfault.com/page/img/avatar/fen.c0d35a56.png'
-    @data['banner_two']['image_url'] = 'http://static.segmentfault.com/page/img/avatar/fen.c0d35a56.png'
+
+    uploader_one = ActsAsImageable::ImageUploader.new
+    uploader_one.store!(params[:image_one])
+
+    uploader_two = ActsAsImageable::ImageUploader.new
+    uploader_two.store!(params[:image_two])
+
+    uploader_three = ActsAsImageable::ImageUploader.new
+    uploader_three.store!(params[:image_three])
+
+    @data['banner_one']['image_url'] = uploader_one.url
+    @data['banner_one']['redirect_to'] = params[:redirect_to_one]
+    @data['banner_two']['image_url'] = uploader_two.url
+    @data['banner_two']['redirect_to'] = params[:redirect_to_two]
+    @data['banner_three']['image_url'] = uploader_three.url
+    @data['banner_three']['redirect_to'] = params[:redirect_to_three]
     File.open(Rails.root.join('config', 'site.yml'), 'wb') { |f| YAML.dump(@data, f) }
+    redirect_to '/admin/banners'
   end
 end
